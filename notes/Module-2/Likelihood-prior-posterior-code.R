865bf8916a3d7f34dfab0d2e62804417d3c686cd
Himanshu Yadav <yadavhimanshu059@gmail.com>
2024-01-30 12:59:08 +0530

    Add files via upload

--- notes/Module-2/Likelihood-prior-posterior-code.R
+++ notes/Module-2/Likelihood-prior-posterior-code.R
@@ -32,3 +32,71 @@ dat.m <- melt(dat,id=("theta"))
 library(ggplot2)
 ggplot(dat.m,aes(x=theta,y=value,group=variable,color=variable))+
   geom_point()+facet_wrap(~variable,scales="free",nrow=3)
+
+##################################
+
+# Data: 350, 300, 200
+# Likelihood assumption:
+# x_ì ~ Normal(mu,sigma)
+# Prior assumptions:
+# sigma=40
+# mu ~ Normal(350,25)
+
+# Choose a lot of equidistant values of mu
+mu <- seq(from=0,to=1000,length=50000)
+sigma <- 40               
+x <- c(350,300,200)    # data
+
+# We need to calculate likelihood corresponding
+# to each value of mu
+
+# Creating an empty likelihood vector
+Likelihood <- rep(NA,50000)
+
+# Calculate the joint likelihood of observing 
+# c(350,300,200) from a particular value of mu
+for(i in 1:length(mu)){
+  Likelihood[i] <- prod(dnorm(x=x,mean=mu[i],sd=sigma))
+}
+Likelihood 
+
+plot(mu,Likelihood)
+
+# Calculate prior densities correspoding to each mu
+prior <- dnorm(mu,mean=350,sd=25)
+plot(mu,prior)
+
+
+unormalized_posterior <- Likelihood*prior
+plot(mu,unormalized_posterior)
+
+ML <- sum(Likelihood*prior)
+posterior <- unormalized_posterior/ML
+plot(mu,posterior)
+
+
+
+# Plot all of them together
+df.post <- data.frame(mu=mu,Prior=prior,
+                      Likelihood=Likelihood,
+                      Posterior=posterior)
+
+head(df.post)
+
+library(reshape2)
+df.post.m <- melt(df.post,id=c("mu"))
+head(df.post.m)
+
+library(ggplot2)
+ggplot(df.post.m,aes(x=mu,y=value,group=variable))+
+  geom_point()+geom_line()+
+  theme_bw()+
+  facet_wrap(~variable,scales = "free_y",nrow = 3)
+
+ggplot(df.post.m,aes(x=mu,y=value,group=variable))+
+  geom_point()+geom_line()+
+  theme_bw()+
+  facet_wrap(~variable,scales = "free_y",nrow = 3)+
+  scale_x_continuous(limits = c(100,500))
+
+
